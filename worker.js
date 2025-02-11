f2baa48a4412c56e5d7d45310519e34bb4740aa8
Student <ljutfi.zenuni@dci-student.org>
2025-02-11 10:09:33 +0100

    worker

---
+++ worker.js
@@ -0,0 +1,9 @@
+class Worker {
+    constructor(name, position) {
+        this.name = name;
+        this.position = position;
+    }
+}
+
+const worker1 = new Worker("sandy", "Cashier");
+console.log(worker1);
