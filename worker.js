8b21e18084ba99bc94dc0d91ee33e541a4361b78
Student <issam.dbiri@dci-student.org>
2025-02-11 10:19:15 +0100

    solution.js

--- worker.js
+++ worker.js
@@ -7,3 +7,17 @@ class Worker {
 
 const worker1 = new Worker("sandy", "Cashier");
 console.log(worker1);
+
+
+
+class Manager extends Worker {
+    constructor(name, branchId) {
+      super(name, "Manager"); 
+      this.branchId = branchId;
+      this.branchWorkers = []; 
+    }
+  }
+
+
+  const manager1 = new Manager("Tina", 1);
+console.log(manager1);
